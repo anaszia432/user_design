e7bf933603e37dce9b080f2a9e11ffa6435a6f31
Anas Zia <73122769+anaszia15@users.noreply.github.com>
2025-09-22 19:21:33 +0530

    space end

--- lib/user_design.dart
+++ lib/user_design.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: BookHistory(
-        title: "Booking History ",
+        title: "Booking History",
       ),
     );
   }
